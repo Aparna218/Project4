fba4947e3f378498bea98d1908b43200f036750f
sbriju <biswasshayan339@gmail.com>
2022-12-12 18:04:01 +0530

    completed3

--- src/controllers/urlController.js
+++ src/controllers/urlController.js
@@ -35,7 +35,14 @@ const createUrl = async (req, res) => {
 //getUrl
 const getUrl = async (req, res) => {
     try {
+        const urlCode = req.params.urlCode;
 
+        if (!shortid.isValid(urlCode)) return res.status(400).send({ status: false, message: `'${urlCode}' this shortUrl is invalid` })
+
+        //existUrl
+        const existUrl = await urlModel.findOne({ urlCode })
+        if (!existUrl) return res.status(404).send({ message: `No url found by this '${urlCode}'.` });
+        return res.status(302).redirect(existUrl.longUrl)
     } catch (err) {
         console.log(err);
         return res.status(500).send({ status: false, error: err.message });
