f244bdf09736d5159dae54ef025f88771d91b4ff
sbriju <117517348+sbriju@users.noreply.github.com>
2022-12-15 12:20:58 +0530

    Update urlController.js

--- src/controllers/urlController.js
+++ src/controllers/urlController.js
@@ -4,13 +4,30 @@ const urlModel = require('../models/urlModel');
 const { isValidUrl, isValidBody } = require('../util/validator');
 const { SET_ASYNC, GET_ASYNC } = require('../caching/redis');
 
+
 //createUrl
 const createUrl = async (req, res) => {
     try {
         const { longUrl } = req.body;
         if (!isValidBody(req.body)) return res.status(400).send({ status: false, message: 'Please enter data on the body.' });
         if (!isValidUrl(longUrl)) return res.status(400).send({ status: false, message: ` '${longUrl}' this url isn't valid.` });
-        if (!validUrl.isUri(longUrl)) return res.status(400).send({ status: false, message: 'Please enter valid url2' });
+        if (!validUrl.isUri(longUrl)) return res.status(400).send({ status: false, message: 'Please enter valid url' });
+
+        //existsCache
+        let existsCache = await GET_ASYNC(`${longUrl}`);
+        existsCache = JSON.parse(existsCache);
+        if (existsCache) {
+            const fromCache = {
+                longUrl: existsCache.longUrl,
+                shortUrl: existsCache.shortUrl,
+                urlCode: existsCache.urlCode
+            };
+            if (existsCache) return res.status(201).send({ status: true, message: 'From cache.', data: fromCache });
+        };
+
+        //existUrl
+        let existUrl = await urlModel.findOne({ longUrl }).select({ longUrl: 1, shortUrl: 1, urlCode: 1, _id: 0 });
+        if (existUrl) return res.status(200).send({ status: true, message: 'From database.', data: existUrl });
 
         let urlCode = shortid.generate().toLowerCase();
         const shortUrl = `http://localhost:3000/${urlCode}`;
@@ -18,8 +35,9 @@ const createUrl = async (req, res) => {
             longUrl: longUrl,
             shortUrl: shortUrl,
             urlCode: urlCode
-        }
+        };
 
+        //newUrl
         const newUrl = await urlModel.create(data);
         const savaData = {
             longUrl: newUrl.longUrl,
@@ -49,7 +67,7 @@ const getUrl = async (req, res) => {
         if (!existUrl) return res.status(404).send({ message: `No url found by this '${urlCode}' shortid.` });
 
         //setCache
-        await SET_ASYNC(`${urlCode}`, JSON.stringify(existUrl));
+        await SET_ASYNC(`${urlCode, existUrl.longUrl}`, JSON.stringify(existUrl));
         return res.status(302).redirect(existUrl.longUrl);
     } catch (err) {
         console.log(err);
@@ -58,3 +76,4 @@ const getUrl = async (req, res) => {
 };
 
 module.exports = { createUrl, getUrl };
+
